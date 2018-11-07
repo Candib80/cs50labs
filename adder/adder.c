865386ac0dfd971347944cf1c1250832c92d5b69
Candice Beaman <candib80@gmail.com>
2018-11-07 11:10:24 -0500

    Create adder.c

---
+++ adder/adder.c
@@ -0,0 +1,9 @@
+#include <stdio.h>
+
+int main(void)
+{
+    // write your main function below this comment
+    
+}
+
+/* write your adder function below this comment - make sure to include your function declaration in the appropriate spot */
